fb961ba7200f8eb58223bb9df269381a509321c5
Srikanth-coder-max <srikanthnarayanasamy60@gmail.com>
2026-06-06 12:09:11 +0530

    Feat: wire gateway interceptor to queue manager service

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, status
 from fastapi.responses import JSONResponse
 from app.core.config import settings
+from app.services.queue_manager import add_to_queue
 import httpx
 
 router = APIRouter()
@@ -13,7 +14,8 @@ async def proxy_gateway(request: Request):
     async with httpx.AsyncClient() as client:
         response = await client.post(target_url, json=payload)
         if response.status_code == 400:
-            # TODO: Tyson payload details ha pathuko 
+            vendor_data_error = response.json()
+            await add_to_queue(payload, vendor_data_error)
     
             return JSONResponse(
                 status_code=status.HTTP_202_ACCEPTED,
